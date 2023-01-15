e519aa95f35d5a3141cbcfef7117469ad772a133
Isac Narcizo Baroni Bazzani <106942461+IsacI@users.noreply.github.com>
2023-01-15 17:51:38 -0300

    Add files via upload

---
+++ import SwitUI.swift
@@ -0,0 +1,15 @@
+import SwitUI
+
+struct contentView: view{
+    var body: some view{
+        ZStrack{
+            FinalView()
+        }
+    }
+}
+
+struct ContentView_Previewa:
+static var previews: some
+contentView()
+}
+}
